8d25ca34ba321767c0be7e3e67fffd205af3a4d9
FatalMistake02 <mistake02.fatal@gmail.com>
2026-01-07 22:07:33 -0500

    Update

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routers import cards, sets
 
-app = FastAPI(title="Cardly API (Supabase Wrapper)")
+app = FastAPI(title="Cardly API")
 
 app.include_router(cards.router)
 app.include_router(sets.router)
